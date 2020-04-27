7398861e27f06d3f119e569537985479c1d7c8a1
poloko <pmohotsithesecond@gmail.com>
2020-04-28 00:39:35 +0200

    bug fixes.

--- app/src/main/java/com/example/grocerylist/GroceryAdapter.java
+++ app/src/main/java/com/example/grocerylist/GroceryAdapter.java
@@ -66,6 +66,7 @@ public class GroceryAdapter extends RecyclerView.Adapter<GroceryAdapter.GroceryV
                                 public void onClick(DialogInterface dialog, int which) {
 
 
+
                                 }
                             });
                             builder.create().show();
